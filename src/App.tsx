435505b26b2994900f26756ed8cc589ec04f0c99
CoderGhost37 <coderghost37@gmail.com>
2024-10-16 12:43:17 +0530

    feat: fetch data according to the checked checkboxes in an interval of 2sec

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,69 @@
-function App() {
-  return (
-    <h1 className='text-xl text-blue-500 font-bold'>Hello World</h1>
-  )
-}
+import { useState, useEffect } from 'react';
 
-export default App
+const App = () => {
+  const [checkedBoxes, setCheckedBoxes] = useState(Array(7).fill(false));
+  const [activeCards, setActiveCards] = useState<number[]>([]);
+  const [cardData, setCardData] = useState(Array(7).fill(''));
+
+  const toggleCheckbox = (index: number) => {
+    const updatedBoxes = [...checkedBoxes];
+    updatedBoxes[index] = !updatedBoxes[index];
+    setCheckedBoxes(updatedBoxes);
+
+    if (updatedBoxes[index]) {
+      setActiveCards(prev => [...prev, index]);
+    } else {
+      setActiveCards(prev => prev.filter(idx => idx !== index));
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async (index: number) => {
+      try {
+        const res = await fetch(`https://navirego-interview.vercel.app/api/letters/${index}`);
+        const data = await res.json();
+        const newLetter = data.letter;
+
+        setCardData(prevData => {
+          const updatedData = [...prevData];
+          updatedData[index] = (updatedData[index] + newLetter).slice(-30);
+          return updatedData;
+        });
+      } catch (error) {
+        alert(`Error fetching data for checkbox ${index}:`);
+      }
+    };
+
+    const intervals = checkedBoxes.map((isChecked, index) => {
+      if (isChecked) {
+        return setInterval(() => fetchData(index), 2000);
+      }
+      return null;
+    });
+
+    return () => {
+      intervals.filter(interval => interval !== null).forEach(interval => clearInterval(interval as Timer));
+    };
+  }, [checkedBoxes]);
+
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-gray-200 py-4 px-4 sm:px-8">
+      <h1 className="text-2xl font-bold mb-4">Navirego Assignment</h1>
+      <div className="flex gap-4 mb-8 mt-4">
+        {checkedBoxes.map((isChecked, index) => (
+          <div key={index} className="flex items-center">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={() => toggleCheckbox(index)}
+              className="mr-2"
+            />
+            <label className='flex items-center'><span className='hidden md:block mr-1 md:text-sm lg:text-base'>Checkbox</span> {index + 1}</label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
